1ef1caaeab3f120dc880b633b4021a1c99d40df4
javiera izquierdo <javieraizquierdo7@gmail.com>
2025-01-14 10:59:13 -0300

    busca paises, capitales y por region, da info al hacer click en ver mas, muestra traducciones en diferentes idiomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -10,6 +13,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+
     SharedModule,
   ],
   providers: [],
